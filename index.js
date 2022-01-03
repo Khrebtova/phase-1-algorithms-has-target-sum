dc690728eef9b9678429c35098a8d3f81e7769fa
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-01-03 11:42:20 -0800

    commit tree bf84355621ae7d710badc976de31fb851eb3b48b

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  // iterate each element of an input array 
+  for ( let i = 0; i < array.length; i ++){
+    // iterate through the rest af an array
+    for ( let j = i + 1; j < array.length; j ++){
+      //get summ of 2 elements and compare to target
+      if ((array[i]+array[j]) === target) return true
+    }
+  }
+  //if didn't find summ of 2 elements what = target return FALSE
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n²)
 */
 
 /* 
   Add your pseudocode here
+  iterate through an array twice . add every element of an array to each oter and compare result to arget input.
 */
 
 /*
@@ -29,6 +41,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([13, 2, 9, 10, 30], 11));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([11, 22, 55, 19, 4, 6, 30], 4));
 }
 
 module.exports = hasTargetSum;
